2973df4ca81b7920b7f3d678da98d364e2c6eb1d
Sameemadhu16 <sameemadhu2003@gmail.com>
2024-04-15 18:30:56 +0530

    assignment operator

--- dataTypes.js
+++ dataTypes.js
@@ -1,3 +1,28 @@
 /* Data Types:
 undefined,null, boolean, string,symbol.number,and object
 */
+
+var myName = "sameera" //going on whole program
+myName = 8  //can change
+
+let ourName = "RAD_Members" //withing the scope
+
+const pi = 3.14 //shold never change
+//pi = 3      give error
+
+//Assignment Operator
+var a;
+var b = 5;
+console.log(a)
+console.log(b)
+
+a = 4;
+console.log(a)
+console.log(b)
+
+b = a;
+console.log(a)
+console.log(b)
+
+//nitializing Variables
+
