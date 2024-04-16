e0006bff0bd72463fc09ac20f66b24178ec4c0e3
Sameemadhu16 <sameemadhu2003@gmail.com>
2024-04-16 08:30:41 +0530

    variables

--- dataTypes.js
+++ dataTypes.js
@@ -24,5 +24,37 @@ b = a;
 console.log(a)
 console.log(b)
 
-//nitializing Variables
+//case sencitive
+//also can increment and decrement
+var myVar = 50;
+myVar++;
+console.log(myVar)
 
+myVar--;
+console.log(myVar)
+
+//quatation mark inside the string
+var myStr ="I am a \"double quated\" string inside \"double quates\""
+console.log(myStr)
+//sigle quatation also can used
+
+//Escape sequence
+/* 
+\' single quote
+\" double quote
+\\ backslash
+\n newline
+\r carriage return
+\t tab
+\b backspace
+\f form feed
+*/
+
+//concatanatimg string with plus operator
+var ourStr = "My name is " + "Sameera Madhuranga."
+console.log(ourStr)
+
+// concatanating strings with plus equal operator
+var str1 = "I come first.";
+str1 += " I come second.";
+console.log(str1) 
