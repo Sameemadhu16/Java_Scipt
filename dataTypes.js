f96bb042b28783f6f7783b9669d1ec4a7ece237e
Sameemadhu16 <sameemadhu2003@gmail.com>
2024-04-16 09:15:27 +0530

    more about variables

--- dataTypes.js
+++ dataTypes.js
@@ -58,3 +58,47 @@ console.log(ourStr)
 var str1 = "I come first.";
 str1 += " I come second.";
 console.log(str1) 
+
+//constructing strings with variables
+var str2 = "Sameera";
+var str3 = "My name is " + str2 +" and I am well."
+console.log(str3)
+
+//Appending variables to string
+var adjective = "worthwhile";
+var str4 = "Learning to code is ";
+str4 += adjective
+console.log(str4)
+
+//Finding length of string
+var firstNameLength = 0;
+var firstName = "Sameera";
+firstNameLength = firstName.length;
+console.log(firstNameLength)
+
+//Bracket notation to find first character in string
+var firstLetterOfFirstName = "";
+firstLetterOfFirstName = firstName[0];
+console.log(firstLetterOfFirstName)
+
+//String Imutability
+var str = "Jello World";
+str[0] = "H";
+console.log(str) // not changed the out put because we must need to change like this
+str = "Hello World"
+console.log(str) // then output is changed
+
+//Find last letter of a string
+var lastLetterOfFirstName = firstName[firstName.length - 1];
+console.log(lastLetterOfFirstName)
+//Also we can find N th to last character in string
+
+//WordBlank
+function wordBlanks(myNoun,myAdjective,myVerb,myAdverb) {
+    var result ="";
+    result += "The " + myAdjective +" "+ myNoun+" " + myVerb + " to the store "+ myAdverb+".";
+
+   return result; 
+}
+console.log(wordBlanks("dog","big","ran","quickly"));
+
